a36ea49e7e4a9ef23001f21ba1975c32c61acc76
kupinggepeng <40017226+kupinggepeng@users.noreply.github.com>
2018-07-04 22:02:52 +0700

    Add files via upload

---
+++ vtt-conv2.py
@@ -0,0 +1,16 @@
+import sys, webvtt
+from pysrt import SubRipFile, SubRipItem
+
+
+def main():
+	# Initialize 
+	vtt , srt , index = webvtt.read(sys.argv[1]) , SubRipFile() , 0
+	for block in vtt:
+		srt.append(SubRipItem(index, start=block.start, end=block.end, text=block.text))
+		index += 1
+	srt.save(sys.argv[1].split('.')[0] +".srt", encoding="utf8")
+	exit(0)
+
+
+if __name__ == '__main__':
+	main()
